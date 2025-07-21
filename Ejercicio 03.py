438e5cd23a5e8d782d08279447fc7b743177e101
Juan Diego <chucjuan508@gmail.com>
2025-07-21 14:52:23 -0800

    Ejercicio 4

--- Ejercicio 03.py
+++ Ejercicio 03.py
@@ -2,16 +2,25 @@
 print(" Sistema de autenticación avanzado con penalización")
 users=["Juan15","pEdro25", "Pabl012" ]
 passwords= [123, 456, 789]
+logged_in= False
 for i in range (3):
     user=input("ingrese su usuario ")
     password=int(input("ingrese su contraseña "))
-    if user == users and password==passwords:
-        print("Boenvenido")
-        print("1. Ver perfil")
-        print("2. Cambiar contraseña")
-        print("3. Cerras Sesión")
-    elif i !=3:
-        print("Usuario o Contraseña incorrecta")
-        print("vuelva a intentarlo")
+    if user in users and password in passwords:
+        user1 = users.index(user)
+        if password == passwords[user1]:
+             print("Boenvenido")
+             print("1. Ver perfil")
+             print("2. Cambiar contraseña")
+             print("3. Cerras Sesión")
+             logged_in= True
+             break
+        else:
+            print("Usuario o Contraseña incorrecta")
+            print("Vuelva a intentarlo ")
     else:
-        print("▲ Acceso Bloqueado▲")
+        print("Usuario o Contraseña incorrecta")
+        print("Vuelva a intentarlo ")
+
+if not logged_in:
+    print("▲ Aceso Bloqueado ▲")
---
+++ Ejercicio 04.py
@@ -0,0 +1,2 @@
+#print("Simulador de facturacion con IVA, descuentos y propina")
+print("no pude con este ejercicio INGE 😓😪")
