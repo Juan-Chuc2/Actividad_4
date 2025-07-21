a8eebd7dfd4f1f8d7259e872309108f9370af6c5
Juan Diego <chucjuan508@gmail.com>
2025-07-20 17:54:41 -0800

    1 # ejercicio 1

--- Actividad 04.py
+++ Actividad 04.py
@@ -1,1 +1,18 @@
 # Ejercicio 01
+print("Bienvenido al simulador de votacion cruzada ")
+name_user= str(input("Ingrese su nombre completo ")).upper()
+dpi1=input("Ingrese su numero de DPI(Documento Personal de Identificacion) ")
+departamentos=str(input("Ingrese su departamento ")).upper()
+year_of_birth= int(input("Ingrese su año de nacimiento (solo el año) "))
+name= len(name_user)
+dpi= len(dpi1)
+if dpi <13 or name<=5:
+    print("Su dpi o nombre es invalido")
+elif departamentos== "ALTA VERAPAZ" or departamentos== "PETEN" :
+    if (2025 - year_of_birth)==17 or (2025 - year_of_birth) >=18:
+        print(f"Bienvenido {name_user}, su centro de votacion es {departamentos}")
+    print("")
+elif  (2025 - year_of_birth) >=18:
+    print(f"Bienvenido {name_user}, su centro de votacion es {departamentos}")
+else:
+    print("no puede votar verifique si a su edad puede votar en su departamento ")
