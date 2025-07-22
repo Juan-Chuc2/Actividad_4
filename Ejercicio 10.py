1c10b6b6ed57dd1f6643e3d0978c69706d614d45
Juan Diego <chucjuan508@gmail.com>
2025-07-21 22:21:38 -0800

    Commit 2 ej10

--- Ejercicio 10.py
+++ Ejercicio 10.py
@@ -7,3 +7,19 @@ print("Segunda fecha:")
 day2 = int(input("Día: "))
 month2 = int(input("Mes: "))
 year2 = int(input("Año: "))
+if year1 > year2:
+    print("La primera fecha es mayor.")
+elif year1 < year2:
+    print("La segunda fecha es mayor.")
+else:
+    if month1 > month2:
+        print("La primera fecha es mayor.")
+    elif month1 < month2:
+        print("La segunda fecha es mayor.")
+    else:
+        if day1 > day2:
+            print("La primera fecha es mayor.")
+        elif day1 < day2:
+            print("La segunda fecha es mayor.")
+        else:
+            print("Ambas fechas son iguales.")
